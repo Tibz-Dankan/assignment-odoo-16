cfd229748224d5200c7721e65285e7472c257733
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-21 16:08:46 +0300

    feat: national ID module setup

---
+++ addons/national_id/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "National ID Application", 'version': '1.2',
+    'category': 'Services/national_id',
+    'sequence': 10,
+    'summary': 'National ID Registration',
+    'website': 'https://www.odoo.com/app/national_id_application',
+}
