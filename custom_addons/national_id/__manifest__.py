d83bc34da96f2a94a72f1c82721519d3d5aef84b
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-21 22:08:25 +0300

    fix: addons_path configured correctly

--- custom_addons/national_id/__manifest__.py
+++ custom_addons/national_id/__manifest__.py
@@ -1,7 +1,9 @@
 {
-    "name": "National ID Application", 'version': '1.2',
+    'name': 'National ID Application',
+    'version': '1.2',
     'category': 'Services/national_id',
     'sequence': 10,
     'summary': 'National ID Registration',
     'website': 'https://www.odoo.com/app/national_id_application',
+     'license': 'LGPL-3',
 }
