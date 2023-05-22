d90b7d6590698db2c92a864f888692e8c2c619f8
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-22 23:13:27 +0300

    feat: config file placed in the odoo folder

--- custom_addons/national_id/__manifest__.py
+++ custom_addons/national_id/__manifest__.py
@@ -1,9 +1,13 @@
 {
     'name': 'National ID Application',
+    'author': 'Tibesigwa Dankan',
     'version': '1.2',
     'category': 'Services/national_id',
     'sequence': 10,
     'summary': 'National ID Registration',
     'website': 'https://www.odoo.com/app/national_id_application',
-     'license': 'LGPL-3',
+    'depends': ['national_id'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
 }
